3934786d49520e272b00cfd0a6456dec23d95492
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-07-03 23:14:10 +0600

    use put method for update

--- src/app/modules/product/product.route.ts
+++ src/app/modules/product/product.route.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/', ProductController.createProduct);
 router.get('/', ProductController.getAllProductFromDB);
 router.get('/:productId', ProductController.getSingleProductFromDB);
-router.patch('/:productId', ProductController.updateProduct);
+router.put('/:productId', ProductController.updateProduct);
 router.delete('/:productId', ProductController.deleteSingleProductFromDB);
 
 export const ProductRoutes = router;
