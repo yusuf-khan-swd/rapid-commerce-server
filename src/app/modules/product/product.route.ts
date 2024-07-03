e8c07e00fc19942335226190c0ede29c8d30695a
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-07-03 19:18:08 +0600

    create a route for update product

--- src/app/modules/product/product.route.ts
+++ src/app/modules/product/product.route.ts
@@ -6,6 +6,7 @@ const router = express.Router();
 router.post('/', ProductController.createProduct);
 router.get('/', ProductController.getAllProductFromDB);
 router.get('/:productId', ProductController.getSingleProductFromDB);
+router.patch('/:productId', ProductController.updateProduct);
 router.delete('/:productId', ProductController.deleteSingleProductFromDB);
 
 export const ProductRoutes = router;
