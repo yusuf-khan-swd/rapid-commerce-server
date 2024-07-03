0632a383004c7e5ce0f6169ace78ce3fc26c58f2
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-07-03 23:35:44 +0600

    add order mongoose schema

--- src/app/modules/order/order.model.ts
+++ src/app/modules/order/order.model.ts
@@ -1,29 +1,16 @@
 import { Schema, model } from 'mongoose';
-import { TInventory, TProduct, TVariant } from './order.interface';
+import { TOrder } from './order.interface';
 
-const variantSchema = new Schema<TVariant>({
-  type: { type: String, required: true },
-  value: { type: String, required: true },
-});
-
-const inventorySchema = new Schema<TInventory>({
-  quantity: { type: Number, required: true },
-  inStock: { type: Boolean, required: true },
-});
-
-const productSchema = new Schema<TProduct>(
+const productSchema = new Schema<TOrder>(
   {
-    name: { type: String, required: true },
-    description: { type: String, required: true },
+    email: { type: String, required: true },
+    productId: { type: String, required: true },
     price: { type: Number, required: true },
-    category: { type: String, required: true },
-    tags: { type: [String], required: true },
-    variants: { type: [variantSchema], required: true },
-    inventory: { type: inventorySchema, required: true },
+    quantity: { type: Number, required: true },
   },
   {
     toJSON: { virtuals: true },
   },
 );
 
-export const Order = model<TProduct>('Order', productSchema);
+export const Order = model<TOrder>('Order', productSchema);
