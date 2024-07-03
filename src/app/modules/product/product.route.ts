602a2ac3423896b45486c8251f5e6a0be1c12e3b
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-07-03 19:01:18 +0600

    update create product post route

--- src/app/modules/product/product.route.ts
+++ src/app/modules/product/product.route.ts
@@ -3,7 +3,7 @@ import { ProductController } from './product.controller';
 
 const router = express.Router();
 
-router.post('/create-product', ProductController.createProduct);
+router.post('/', ProductController.createProduct);
 router.get('/', ProductController.getAllProductFromDB);
 router.get('/:id', ProductController.getSingleProductFromDB);
 router.delete('/:id', ProductController.deleteSingleProductFromDB);
