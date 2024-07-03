537fdd19992f7750d21e2a2a1a67105baa41d7d8
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-07-03 21:05:42 +0600

    Add product interface

--- src/app/modules/product/product.interface.ts
+++ src/app/modules/product/product.interface.ts
@@ -1,42 +1,19 @@
-export type TUserName = {
-  firstName: string;
-  middleName?: string;
-  lastName: string;
+export type TVariant = {
+  type: string;
+  value: string;
 };
 
-export type TGuardian = {
-  fatherName: string;
-  fatherOccupation: string;
-  fatherContactNo: string;
-  motherName: string;
-  motherOccupation: string;
-  motherContactNo: string;
+export type TInventory = {
+  quantity: number;
+  inStock: boolean;
 };
-
-export type TLocalGuardian = {
-  name: string;
-  occupation: string;
-  contactNo: string;
-  address: string;
-};
-
-type TBloodGroup = 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
 
 export type TProduct = {
-  id: string;
-  password: string;
-  name: TUserName;
-  gender: 'male' | 'female' | 'other';
-  dateOfBirth?: string;
-  email: string;
-  contactNo: string;
-  emergencyContactNo: string;
-  bloodGroup?: TBloodGroup;
-  presentAddress: string;
-  permanentAddress: string;
-  guardian: TGuardian;
-  localGuardian: TLocalGuardian;
-  profileImg?: string;
-  isActive: 'active' | 'blocked';
-  isDeleted: boolean;
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: string[];
+  variants: TVariant[];
+  inventory: TInventory;
 };
