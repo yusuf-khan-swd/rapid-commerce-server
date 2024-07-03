f67d863540fb05715ba86e4e422bef0d285c6bf3
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-07-03 18:43:31 +0600

    remove all the pre and post hook middleware from model

--- src/app/modules/product/product.model.ts
+++ src/app/modules/product/product.model.ts
@@ -1,13 +1,11 @@
-import bcrypt from 'bcrypt';
 import { Schema, model } from 'mongoose';
-import config from '../../config';
 import {
   StudentModel,
   TGuardian,
   TLocalGuardian,
   TStudent,
   TUserName,
-} from './student.interface';
+} from './product.interface';
 
 const userNameSchema = new Schema<TUserName>({
   firstName: {
@@ -129,59 +127,5 @@ const studentSchema = new Schema<TStudent, StudentModel>(
     toJSON: { virtuals: true },
   },
 );
-
-studentSchema.virtual('fullName').get(function () {
-  const firstName: string = this.name.firstName;
-  const lastName: string = this.name.lastName;
-  const middleName: string | undefined = this?.name?.middleName;
-
-  return middleName
-    ? `${firstName} ${middleName} ${lastName}`
-    : `${firstName} ${lastName}`;
-});
-
-studentSchema.pre('save', async function (next) {
-  // eslint-disable-next-line @typescript-eslint/no-this-alias
-  const user = this;
-
-  user.password = await bcrypt.hash(
-    user.password,
-    Number(config.bcrypt_salt_round),
-  );
-
-  next();
-});
-
-studentSchema.post('save', function (doc, next) {
-  doc.password = '';
-
-  next();
-});
-
-studentSchema.pre('find', function (next) {
-  this.find({ isDeleted: { $ne: true } });
-  next();
-});
-
-studentSchema.pre('findOne', function (next) {
-  this.find({ isDeleted: { $ne: true } });
-  next();
-});
-
-studentSchema.pre('aggregate', function (next) {
-  this.pipeline().unshift({ $match: { isDeleted: { $ne: true } } });
-  next();
-});
-
-studentSchema.statics.isUserExist = async function (studentId: string) {
-  const isUserExist = await Student.findOne({ id: studentId });
-  return isUserExist;
-};
-
-// * In here is the custom instance method
-// studentSchema.methods.isUserExist = async function (id: string) {
-//   const isUserExist = await Student.findOne({ id });
-//   return isUserExist;
-// };
 
 export const Student = model<TStudent, StudentModel>('Student', studentSchema);
