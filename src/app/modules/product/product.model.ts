40d4865dc72e4233cbc8e2e69ee41700719fc406
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-07-03 21:05:53 +0600

    add product model

--- src/app/modules/product/product.model.ts
+++ src/app/modules/product/product.model.ts
@@ -1,124 +1,25 @@
 import { Schema, model } from 'mongoose';
-import {
-  TGuardian,
-  TLocalGuardian,
-  TProduct,
-  TUserName,
-} from './product.interface';
+import { TInventory, TProduct, TVariant } from './product.interface';
 
-const userNameSchema = new Schema<TUserName>({
-  firstName: {
-    type: String,
-    required: [true, 'firstName is required'],
-    trim: true,
-    maxlength: [20, 'firstName can not be more then 20 character'],
-  },
-  middleName: { type: String, trim: true },
-  lastName: {
-    type: String,
-    required: [true, 'lastName is required'],
-    trim: true,
-    maxlength: [20, 'lastName can not be more then 20 character'],
-  },
+const variantSchema = new Schema<TVariant>({
+  type: { type: String, required: true },
+  value: { type: String, required: true },
 });
 
-const guardianSchema = new Schema<TGuardian>({
-  fatherName: {
-    type: String,
-    required: [true, 'fatherName is required'],
-    trim: true,
-  },
-  fatherOccupation: {
-    type: String,
-    required: [true, 'fatherOccupation is required'],
-  },
-  fatherContactNo: {
-    type: String,
-    required: [true, 'fatherContactNo is required'],
-  },
-  motherName: { type: String, required: [true, 'motherName is required'] },
-  motherOccupation: {
-    type: String,
-    required: [true, 'motherOccupation is required'],
-  },
-  motherContactNo: {
-    type: String,
-    required: [true, 'motherContactNo is required'],
-  },
-});
-
-const localGuardianSchema = new Schema<TLocalGuardian>({
-  name: { type: String, required: [true, 'name is required'] },
-  occupation: { type: String, required: [true, 'occupation is required'] },
-  contactNo: { type: String, required: [true, 'contactNo is required'] },
-  address: { type: String, required: [true, 'address is required'] },
+const inventorySchema = new Schema<TInventory>({
+  quantity: { type: Number, required: true },
+  inStock: { type: Boolean, required: true },
 });
 
 const productSchema = new Schema<TProduct>(
   {
-    id: {
-      type: String,
-      required: [true, 'Student ID is required'],
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: [true, 'Password is required'],
-      maxlength: [20, 'Password can not be more than 20 characters'],
-    },
-    name: { type: userNameSchema, required: [true, 'name is required'] },
-    gender: {
-      type: String,
-      enum: {
-        values: ['male', 'female', 'other'],
-        message: '{VALUE} is not valid gender',
-      },
-      required: [true, 'gender is required'],
-    },
-    dateOfBirth: { type: String },
-    email: {
-      type: String,
-      required: [true, 'email is required'],
-      unique: true,
-    },
-    contactNo: { type: String, required: [true, 'contactNo is required'] },
-    emergencyContactNo: {
-      type: String,
-      required: [true, 'emergencyContactNo is required'],
-    },
-    bloodGroup: {
-      type: String,
-      enum: {
-        values: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
-        message: '{VALUE} is not valid blood group',
-      },
-    },
-    presentAddress: {
-      type: String,
-      required: [true, 'presentAddress is required'],
-    },
-    permanentAddress: {
-      type: String,
-      required: [true, 'permanentAddress is required'],
-    },
-    guardian: {
-      type: guardianSchema,
-      required: [true, 'guardian is required'],
-    },
-    localGuardian: {
-      type: localGuardianSchema,
-      required: [true, 'localGuardian is required'],
-    },
-    profileImg: { type: String },
-    isActive: {
-      type: String,
-      enum: {
-        values: ['active', 'blocked'],
-        message: '{VALUE} is not valid status',
-      },
-      default: 'active',
-    },
-    isDeleted: { type: Boolean, default: false },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: { type: String, required: true },
+    tags: { type: [String], required: true },
+    variants: { type: [variantSchema], required: true },
+    inventory: { type: inventorySchema, required: true },
   },
   {
     toJSON: { virtuals: true },
