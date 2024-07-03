8886ebd9d50531329e6360de656bd7ae0872f576
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-07-03 23:35:26 +0600

    add order interface

--- src/app/modules/order/order.interface.ts
+++ src/app/modules/order/order.interface.ts
@@ -1,19 +1,6 @@
-export type TVariant = {
-  type: string;
-  value: string;
-};
-
-export type TInventory = {
-  quantity: number;
-  inStock: boolean;
-};
-
-export type TProduct = {
-  name: string;
-  description: string;
+export type TOrder = {
+  email: string;
+  productId: string;
   price: number;
-  category: string;
-  tags: string[];
-  variants: TVariant[];
-  inventory: TInventory;
+  quantity: number;
 };
