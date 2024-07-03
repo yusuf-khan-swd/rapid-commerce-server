178328a93b1be95e9eb6dc29155a8df2da47a627
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-07-03 21:06:05 +0600

    add product validation

--- src/app/modules/product/product.validation.ts
+++ src/app/modules/product/product.validation.ts
@@ -1,50 +1,23 @@
 import { z } from 'zod';
 
-const userNameSchema = z.object({
-  firstName: z
-    .string()
-    .min(1)
-    .max(20)
-    .refine((value) => /^[A-Z]/.test(value), {
-      message: 'First Name must start with a capital letter',
-    }),
-  middleName: z.string().optional(),
-  lastName: z.string(),
+const variantSchema = z.object({
+  type: z.string(),
+  value: z.string(),
 });
 
-const guardianSchema = z.object({
-  fatherName: z.string(),
-  fatherOccupation: z.string(),
-  fatherContactNo: z.string(),
-  motherName: z.string(),
-  motherOccupation: z.string(),
-  motherContactNo: z.string(),
-});
-
-const localGuardianSchema = z.object({
-  name: z.string(),
-  occupation: z.string(),
-  contactNo: z.string(),
-  address: z.string(),
+const inventorySchema = z.object({
+  quantity: z.number(),
+  inStock: z.boolean(),
 });
 
 const productValidationSchema = z.object({
-  id: z.string(),
-  password: z.string().max(20),
-  name: userNameSchema,
-  gender: z.enum(['male', 'female', 'other']),
-  dateOfBirth: z.string(),
-  email: z.string().email(),
-  contactNo: z.string(),
-  emergencyContactNo: z.string(),
-  bloodGroup: z.enum(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']),
-  presentAddress: z.string(),
-  permanentAddress: z.string(),
-  guardian: guardianSchema,
-  localGuardian: localGuardianSchema,
-  profileImg: z.string().optional(),
-  isActive: z.enum(['active', 'blocked']).default('active'),
-  isDeleted: z.boolean().optional().default(false),
+  name: z.string(),
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  tags: z.array(z.string()),
+  variants: z.array(variantSchema),
+  inventory: inventorySchema,
 });
 
 export default productValidationSchema;
