e529584314ff97a9ed4f4fcfe00f666fc7e9ea38
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-07-04 10:59:50 +0600

    Remove route of getSingleOrderFromDB,  updateOrder,  deleteSingleOrderFromDB

--- src/app/modules/order/order.route.ts
+++ src/app/modules/order/order.route.ts
@@ -5,8 +5,5 @@ const router = express.Router();
 
 router.post('/', OrderController.createOrder);
 router.get('/', OrderController.getAllOrderFromDB);
-router.get('/:orderId', OrderController.getSingleOrderFromDB);
-router.put('/:orderId', OrderController.updateOrder);
-router.delete('/:orderId', OrderController.deleteSingleOrderFromDB);
 
 export const OrderRoutes = router;
